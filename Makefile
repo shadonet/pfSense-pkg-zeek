70434bcbc7363b2417736639cb51f0e7759a7e41
shadonet <ingprs01@gmail.com>
2018-01-27 01:01:15 +0100

    Refactor code style

--- Makefile
+++ Makefile
@@ -23,39 +23,39 @@ SUB_LIST=	PORTNAME=${PORTNAME}
 do-extract:
 	${MKDIR} ${WRKSRC}
 
-do-install:
+	do-install:
 	${MKDIR} ${STAGEDIR}${PREFIX}/pkg
 	${MKDIR} ${STAGEDIR}${PREFIX}/www
 	${MKDIR} ${STAGEDIR}${PREFIX}/www/shortcuts
 	${MKDIR} ${STAGEDIR}/etc/inc/priv
 	${MKDIR} ${STAGEDIR}${DATADIR}
 	${INSTALL_DATA} -m 0644 ${FILESDIR}${PREFIX}/pkg/bro.xml \
-		${STAGEDIR}${PREFIX}/pkg
+	${STAGEDIR}${PREFIX}/pkg
 	${INSTALL_DATA} ${FILESDIR}${PREFIX}/pkg/bro.inc \
-		${STAGEDIR}${PREFIX}/pkg
+	${STAGEDIR}${PREFIX}/pkg
 	${INSTALL_DATA} ${FILESDIR}${PREFIX}/pkg/bro_broctl.xml \
-		${STAGEDIR}${PREFIX}/pkg
+	${STAGEDIR}${PREFIX}/pkg
 	${INSTALL_DATA} ${FILESDIR}${PREFIX}/pkg/bro_cluster.xml \
-		${STAGEDIR}${PREFIX}/pkg
+	${STAGEDIR}${PREFIX}/pkg
 	${INSTALL_DATA} ${FILESDIR}${PREFIX}/pkg/bro_script.xml \
-		${STAGEDIR}${PREFIX}/pkg
+	${STAGEDIR}${PREFIX}/pkg
 	${INSTALL_DATA} ${FILESDIR}${PREFIX}/pkg/bro_log.xml \
-		${STAGEDIR}${PREFIX}/pkg
+	${STAGEDIR}${PREFIX}/pkg
 	${INSTALL_DATA} ${FILESDIR}${PREFIX}/pkg/bro_sync.xml \
-		${STAGEDIR}${PREFIX}/pkg
+	${STAGEDIR}${PREFIX}/pkg
 	${INSTALL_DATA} ${FILESDIR}${PREFIX}/www/shortcuts/pkg_bro.inc \
-		${STAGEDIR}${PREFIX}/www/shortcuts
+	${STAGEDIR}${PREFIX}/www/shortcuts
 	${INSTALL_DATA} ${FILESDIR}/etc/inc/priv/bro.priv.inc \
-		${STAGEDIR}/etc/inc/priv
+	${STAGEDIR}/etc/inc/priv
 	${INSTALL_DATA} ${FILESDIR}${PREFIX}/www/bro_alerts.php \
-		${STAGEDIR}${PREFIX}/www
+	${STAGEDIR}${PREFIX}/www
 	${INSTALL_DATA} ${FILESDIR}${PREFIX}/www/bro_alert_data.php \
-		${STAGEDIR}${PREFIX}/www
+	${STAGEDIR}${PREFIX}/www
 	${INSTALL_DATA} ${FILESDIR}${PREFIX}/www/select_box_file.php \
-		${STAGEDIR}${PREFIX}/www
+	${STAGEDIR}${PREFIX}/www
 	${INSTALL_DATA} ${FILESDIR}${DATADIR}/info.xml \
-		${STAGEDIR}${DATADIR}
+	${STAGEDIR}${DATADIR}
 	@${REINPLACE_CMD} -i '' -e "s|%%PKGVERSION%%|${PKGVERSION}|" \
-		${STAGEDIR}${DATADIR}/info.xml
+	${STAGEDIR}${DATADIR}/info.xml
 
-.include <bsd.port.mk>
+	.include <bsd.port.mk>
--- files/usr/local/www/select_box_file.php
+++ files/usr/local/www/select_box_file.php
@@ -2,41 +2,33 @@
 //list_by_ext: returns an array containing an alphabetic list of files in the specified directory ($path) with a file extension that matches $extension
 
 function list_by_ext($extension, $path){
-    $list = array(); //initialise a variable
-    $dir_handle = @opendir($path) or die("Unable to open $path"); //attempt to open path
-    while($file = readdir($dir_handle)){ //loop through all the files in the path
-        if($file == "." || $file == ".."){continue;} //ignore these
-        $filename = explode(".",$file); //seperate filename from extenstion
-        //var_dump($filename);
-        $cnt = count($filename); $cnt--; $ext = $filename[$cnt]; //as above
-        if(strtolower($ext) == strtolower($extension)){ //if the extension of the file matches the extension we are looking for...
-            array_push($list, $file); //...then stick it onto the end of the list array
-        }
+  $list = array(); //initialise a variable
+  $dir_handle = @opendir($path) or die("Unable to open $path"); //attempt to open path
+  while($file = readdir($dir_handle)){ //loop through all the files in the path
+    if($file == "." || $file == ".."){continue;} //ignore these
+    $filename = explode(".",$file); //seperate filename from extenstion
+    $cnt = count($filename); $cnt--; $ext = $filename[$cnt];
+    if(strtolower($ext) == strtolower($extension)){ //if the extension of the file matches the extension we are looking for...
+      array_push($list, $file); //...then stick it onto the end of the list array
     }
-    if($list[0]){ //...if matches were found...
+  }
+  if($list[0]){ //...if matches were found...
     return $list; //...return the array
-    } else {//otherwise...
+  } else {//otherwise...
     return false;
-    }
+  }
 }
 
 if($_POST['x'])
 {
-	$x = $_POST['x'];
+  $x = $_POST['x'];
   $current = list_by_ext("log","/usr/local/logs/current");
   $number = count($current);
-
   if ($x==1 || $number>$x) {
-
-  foreach ($current as $value) {
-    # code...
-   echo '<option value="'.$value.'">'.$value.'</option>';
+    foreach ($current as $value) {
+      echo '<option value="'.$value.'">'.$value.'</option>';
+    }
   }
-
-  }
-
-
 }
-
 
 ?>
